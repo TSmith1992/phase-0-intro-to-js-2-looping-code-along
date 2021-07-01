f4f86eeb0c044be2d0307eef3fe219b39b2fabfd
Tarique Smith <tpsmith1992@gmail.com>
2021-07-01 14:00:09 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names,event) {
+    let emptyArray=[];
+    for (let i=0; i<names.length;i++) {
+        emptyArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return emptyArray;
+}
+
+function countDown(positiveNumber) {
+    while(positiveNumber>=0) {
+        console.log(positiveNumber);
+        positiveNumber--;
+    }
+}
